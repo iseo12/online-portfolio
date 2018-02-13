964a032ef23b9f5f73b6ac2232e9b06c55d505f5
Isaac Seo <iseo12@gmail.com>
2018-02-12 19:58:12 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('.projects').hover( function() {
+        $(this).find('figcaption').fadeIn(300);
+    }, function() {
+        $(this).find('figcaption').fadeOut(100);
+    });
+		$('.menuContainer').on('click', function() {
+			$('.icon_bar, .menuContainer, .modal, .navigation li').toggleClass('open');
+		});
+});
+
+var navHeight = $('.top_bar').outerHeight();
+console.log(navHeight);
+
+$('ul li').click(function (e) {
+	e.preventDefault();
+	$(this).addClass('selected').siblings().removeClass('selected');
+	var targetContent = $(this).data('navigation-item');
+	var topPosition = $('.content-' + targetContent).offset().top;
+	console.log(topPosition);
+	console.log(targetContent);
+	$('body,html').animate({
+		scrollTop: topPosition
+	});
+	$('.icon_bar, .menuContainer, .modal, .navigation li').toggleClass('open')
+});
